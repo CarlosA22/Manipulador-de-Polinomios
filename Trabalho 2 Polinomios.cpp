f421b6fb307a1cea2161dc524fa2bb4b300af1a7
Carlos Serena <cserena@edu.univali.br>
2023-10-08 21:01:27 -0300

    revert all

--- Trabalho 2 Polinomios.cpp
+++ Trabalho 2 Polinomios.cpp
@@ -12,116 +12,8 @@ struct Monomio
 
 struct Polinomio
 {
-    vector<Monomio> lista; // Use a vector to store monomios
+    
 };
-
-// Function to add two polynomials
-/*Polinomio somaPolinomios(const Polinomio& p1, const Polinomio& p2)
-{
-    Polinomio resultado;
-    int i = 0, j = 0;
-
-    while (i < p1.lista.size() && j < p2.lista.size())
-    {
-        if (p1.lista[i].expoente == p2.lista[j].expoente && p1.lista[i].incognita == p2.lista[j].incognita)
-        {
-            // Monomials have the same exponent and incognita, so add their coefficients
-            Monomio monomio;
-            monomio.coeficiente = p1.lista[i].coeficiente + p2.lista[j].coeficiente;
-            monomio.expoente = p1.lista[i].expoente;
-            monomio.incognita = p1.lista[i].incognita;
-            resultado.lista.push_back(monomio);
-            i++;
-            j++;
-        }
-        else if (p1.lista[i].expoente > p2.lista[j].expoente || (p1.lista[i].expoente == p2.lista[j].expoente && p1.lista[i].incognita > p2.lista[j].incognita))
-        {
-            // Add the monomial from p1 to the result
-            resultado.lista.push_back(p1.lista[i]);
-            i++;
-        }
-        else
-        {
-            // Add the monomial from p2 to the result
-            resultado.lista.push_back(p2.lista[j]);
-            j++;
-        }
-    }
-
-    // If there are remaining monomials in p1 or p2, add them to the result
-    while (i < p1.lista.size())
-    {
-        resultado.lista.push_back(p1.lista[i]);
-        i++;
-    }
-
-    while (j < p2.lista.size())
-    {
-        resultado.lista.push_back(p2.lista[j]);
-        j++;
-    }
-
-    return resultado;
-}
-*/
-
-Polinomio somaPolinomios(const Polinomio& p1, const Polinomio& p2)
-{
-    Polinomio resultado;
-    int i = 0, j = 0;
-
-    while (i < p1.lista.size() && j < p2.lista.size())
-    {
-        if (p1.lista[i].expoente == p2.lista[j].expoente && p1.lista[i].incognita == p2.lista[j].incognita)
-        {
-            // Monomials have the same exponent and incognita, so add their coefficients
-            Monomio monomio;
-            monomio.coeficiente = p1.lista[i].coeficiente + p2.lista[j].coeficiente;
-            monomio.expoente = p1.lista[i].expoente;
-            monomio.incognita = p1.lista[i].incognita;
-            resultado.lista.push_back(monomio);
-            i++;
-            j++;
-        }
-        else if (p1.lista[i].expoente > p2.lista[j].expoente || (p1.lista[i].expoente == p2.lista[j].expoente && p1.lista[i].incognita > p2.lista[j].incognita))
-        {
-            // Add the monomial from p1 to the result
-            resultado.lista.push_back(p1.lista[i]);
-            i++;
-        }
-        else
-        {
-            // Add the monomial from p2 to the result
-            resultado.lista.push_back(p2.lista[j]);
-            j++;
-        }
-    }
-
-    // If there are remaining monomials in p1 or p2, add them to the result
-    while (i < p1.lista.size())
-    {
-        resultado.lista.push_back(p1.lista[i]);
-        i++;
-    }
-
-    while (j < p2.lista.size())
-    {
-        resultado.lista.push_back(p2.lista[j]);
-        j++;
-    }
-
-    return resultado;
-}
-
-
-void imprimir(const Polinomio& p)
-{
-    for (const Monomio& monomio : p.lista)
-    {
-        cout << monomio.coeficiente << monomio.incognita << "^" << monomio.expoente << " ";
-    }
-    cout << endl;
-}
 
 int main()
 {
@@ -146,35 +38,6 @@ int main()
         {
             system("cls");
 
-            Polinomio p1, p2;
-            cout << "Digite o primeiro polinomio (coeficiente incognita^expoente, digite 0 para terminar):\n";
-            while (true)
-            {
-                Monomio monomio;
-                cin >> monomio.coeficiente;
-                if (monomio.coeficiente == 0)
-                    break;
-                cin >> monomio.incognita;
-                cin >> monomio.expoente;
-                p1.lista.push_back(monomio);
-            }
-
-            cout << "Digite o segundo polinomio (coeficiente incognita^expoente, digite 0 para terminar):\n";
-            while (true)
-            {
-                Monomio monomio;
-                cin >> monomio.coeficiente;
-                if (monomio.coeficiente == 0)
-                    break;
-                cin >> monomio.incognita;
-                cin >> monomio.expoente;
-                p2.lista.push_back(monomio);
-            }
-
-            Polinomio resultado = somaPolinomios(p1, p2);
-
-            cout << "Resultado: ";
-            imprimir(resultado);
             break;
         }
         case 2:
